3b46810fed31a634e0d98ef93171c77eb57d3d20
Irina <irunchon@192.168.1.53>
2023-04-14 17:03:22 +0300

    - unused comments

--- internal/app/commands/commander.go
+++ internal/app/commands/commander.go
@@ -16,15 +16,3 @@ func NewCommander(bot *tgbotapi.BotAPI, productService service.Service) *Command
 		Service: productService,
 	}
 }
-
-//type Commander interface {
-//	Help(inputMsg *tgbotapi.Message) tgbotapi.MessageConfig
-//	Get(inputMsg *tgbotapi.Message) tgbotapi.MessageConfig
-//	List(inputMsg *tgbotapi.Message) tgbotapi.MessageConfig
-//	Delete(inputMsg *tgbotapi.Message) tgbotapi.MessageConfig
-//
-//	New(inputMsg *tgbotapi.Message) tgbotapi.MessageConfig  // return error not implemented
-//	Edit(inputMsg *tgbotapi.Message) tgbotapi.MessageConfig // return error not implemented
-//}
-
-//func NewCommander(bot *tgbotapi.BotAPI, service service.InMemoryService) Commander {}
